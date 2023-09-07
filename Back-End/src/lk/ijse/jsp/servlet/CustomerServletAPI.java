62dbe5fee97b2fbedaac89c7f75d08dd1aa6632c
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-08 03:42:52 +0530

    Update

--- Back-End/src/lk/ijse/jsp/servlet/CustomerServletAPI.java
+++ Back-End/src/lk/ijse/jsp/servlet/CustomerServletAPI.java
@@ -1,11 +1,33 @@
 package lk.ijse.jsp.servlet;
 
+import com.mysql.cj.xdevapi.JsonArray;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
 
 @WebServlet(urlPatterns = {"/cusotmer"})
 public class CustomerServletAPI extends HttpServlet {
     public CustomerServletAPI(){
         System.out.println("Customer Sever Invoked");
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.cj.jdbc");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/assignment","root","1234");
+            PreparedStatement pstm = connection.prepareStatement("select * customer");
+            ResultSet rst = pstm.executeQuery();
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
