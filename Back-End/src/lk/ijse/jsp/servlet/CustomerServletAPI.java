87564d85a3ca90ee8bca40153389c414711d7936
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-08 03:33:16 +0530

    Constructor Calling

--- Back-End/src/lk/ijse/jsp/servlet/CustomerServletAPI.java
+++ Back-End/src/lk/ijse/jsp/servlet/CustomerServletAPI.java
@@ -1,8 +1,11 @@
 package lk.ijse.jsp.servlet;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
 @WebServlet(urlPatterns = {"/cusotmer"})
-public class CustomerServletAPI {
-
+public class CustomerServletAPI extends HttpServlet {
+    public CustomerServletAPI(){
+        System.out.println("Customer Sever Invoked");
+    }
 }
